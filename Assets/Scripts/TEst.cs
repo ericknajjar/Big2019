c4ad010ce00ef7224d015a937f68a2d1c8e6e73b
Erick Najjar <enajjar@gazeus.com>
2019-06-25 13:09:03 -0300

    perfect animations

--- Assets/Scripts/TEst.cs
+++ Assets/Scripts/TEst.cs
@@ -14,9 +14,16 @@ public class TEst : MonoBehaviour
     void Update()
     {
         _animator.ResetTrigger("punch");
+        _animator.ResetTrigger("hadouken");
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _animator.SetTrigger("punch");
+        }
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _animator.SetTrigger("hadouken");
         }
     }
 }
