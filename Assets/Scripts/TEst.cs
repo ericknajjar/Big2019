d83ca61e114a0ef7390604d2e8a3e3c6e56e8a5b
Erick Najjar <enajjar@gazeus.com>
2019-06-25 12:58:33 -0300

    punch animation

---
+++ Assets/Scripts/TEst.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEst : MonoBehaviour
+{
+    Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        _animator.ResetTrigger("punch");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _animator.SetTrigger("punch");
+        }
+    }
+}
