86cbcc2c30ccb66820a7231768216cf84ccf9ac8
Erick <enajjar@gazeus.com.br>
2019-06-26 17:29:12 -0300

    criei exemplo de duplo click

---
+++ Assets/Scripts/DoubleClickDetector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class DoubleClickDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var clickStream = Observable.EveryUpdate()
+    .Where(_ => Input.GetMouseButtonDown(0));
+
+        clickStream.Buffer(TimeSpan.FromMilliseconds(250), 2)
+            .Where(xs => xs.Count >= 2)
+            .Subscribe(xs => Debug.Log("DoubleClick Detected! Count:" + xs.Count));
+    }
+
+
+}
--- Assets/Scripts/ReactiveInput.cs
+++ Assets/Scripts/ReactiveInput.cs
@@ -11,7 +11,7 @@ public partial class ReactiveInput : MonoBehaviour
     private void Awake()
     {
         _animator = GetComponent<Animator>();
-     //  Time.timeScale = 0.3f;
+        Time.timeScale = 0.3f;
     }
 
     void Start()
@@ -22,33 +22,37 @@ public partial class ReactiveInput : MonoBehaviour
 
         punch.Where(_ => _.Pressed).Subscribe(_ => _animator.SetTrigger("punch"));
         punch.Where(_ => !_.Pressed).Subscribe(_ => _animator.ResetTrigger("punch"));
-        crouch.Subscribe(_ => _animator.SetBool("crouched",_.Pressed));
+        crouch.Subscribe(_ => _animator.SetBool("crouched", _.Pressed));
         walk.Subscribe(_ => _animator.SetBool("walking", _.Pressed));
 
         var hadoukenStream = SequenceDetector(punch.Merge(crouch).Merge(walk), 0.3f, InputSequences._hadoukenSequence, InputSequences._alternativeHadoukenSequence);
-        var sheikoHaduken = hadoukenStream.SelectMany(_ =>hadoukenStream.TakeUntil(Observable.Timer(TimeSpan.FromSeconds(0.9f))).Take(1));
+        var sheikoHaduken = hadoukenStream.SelectMany(_ => hadoukenStream.TakeUntil(Observable.Timer(TimeSpan.FromSeconds(0.9f))).Take(1));
 
-        hadoukenStream.Subscribe(_=> {
-            
+        hadoukenStream.Subscribe(_ =>
+        {
+
             _animator.SetTrigger("hadouken");
         });
 
-        sheikoHaduken.Subscribe(_ => {
+        sheikoHaduken.Subscribe(_ =>
+        {
             _animator.ResetTrigger("hadouken");
             _animator.SetTrigger("cancelHadouken");
             _animator.SetTrigger("sheikoHadouken");
         });
     }
 
-    IObservable<Unit> SequenceDetector(IObservable<KeyEvent> source, float windowInSeconds,params IList<KeyEvent>[] sequences)
+    IObservable<Unit> SequenceDetector(IObservable<KeyEvent> source, float windowInSeconds, params IList<KeyEvent>[] sequences)
     {
-        return sequences.Select(sequence => {
+        return sequences.Select(sequence =>
+        {
 
             var startWindow = source.Where(_ => _.Equals(sequence.First())).Share();
 
             var sequenceWithoutFirst = sequence.Skip(1);
 
-            return startWindow.SelectMany(keyEvent => {
+            return startWindow.SelectMany(keyEvent =>
+            {
 
                 return source.Buffer(TimeSpan.FromSeconds(windowInSeconds), sequenceWithoutFirst.Count())
                 .TakeUntil(startWindow)
@@ -61,7 +65,7 @@ public partial class ReactiveInput : MonoBehaviour
 
     static IObservable<KeyEvent> CreateKeyStream(KeyCode key)
     {
-        var down = Observable.EveryUpdate().Where(_ => Input.GetKeyDown(key)).Select(_=> new KeyEvent(key,true));
+        var down = Observable.EveryUpdate().Where(_ => Input.GetKeyDown(key)).Select(_ => new KeyEvent(key, true));
         var up = Observable.EveryUpdate().Where(_ => Input.GetKeyUp(key)).Select(_ => new KeyEvent(key, false));
 
         return down.Merge(up).Share();
